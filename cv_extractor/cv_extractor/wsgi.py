f713f99f4d9ad891b7c9c616185b0a02ba19c261
Miteshnable <miteshnable@gmail.com>
2024-04-15 13:47:13 +0530

    vercel settings added

--- cv_extractor/cv_extractor/wsgi.py
+++ cv_extractor/cv_extractor/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cv_extractor.settings')
 
 application = get_wsgi_application()
+
+app= application
